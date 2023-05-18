1f8f05a8aeba91e607d0efe76f1e107feecc943b
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-05-18 18:29:01 +0300

    added signup and some design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Main from './pages/Main';
-
+import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="app">
+    <div className="app__body">
+      <Header/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
--- client/src/controllers/user.controller.ts
+++ client/src/controllers/user.controller.ts
@@ -1,5 +1,13 @@
-class UserController{
+import Axios from "axios";
+import apiPath from "../api-path";
 
+Axios.defaults.withCredentials = true;
+
+class UserController{
+    async create(data: object){
+        const response = await Axios.post(`${apiPath}/users/create`, data);
+        return response;
+    }
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
