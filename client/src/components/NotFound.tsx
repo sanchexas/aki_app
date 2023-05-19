ff9e8525f8af83c68bb37b23be1056d5f702ed49
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-05-19 12:32:52 +0300

    signup error ahndle

---
+++ client/src/components/NotFound.tsx
@@ -0,0 +1,7 @@
+const NotFound = () =>{
+    return(
+        <h1>404 - страница не найдена</h1>
+    )
+}
+
+export default NotFound;
--- client/src/controllers/user.controller.ts
+++ client/src/controllers/user.controller.ts
@@ -1,12 +1,31 @@
-import Axios from "axios";
+import Axios, { AxiosError } from "axios";
 import apiPath from "../api-path";
+import Cookies from "universal-cookie";
 
 Axios.defaults.withCredentials = true;
-
+type User = {
+    email: string
+    middlename: string
+    name: string
+    phone: string
+    second_name: string
+}
 class UserController{
     async create(data: object){
-        const response = await Axios.post(`${apiPath}/users/create`, data);
-        return response;
+        let result: any;
+        await Axios.post(`${apiPath}/users/create`, data).then((response)=>{
+            const userResult: User = response.data;
+            if(response.status === 200){
+                result = response.status;
+                const cookies = new Cookies();
+                cookies.set('email', userResult.email);
+            } // прописать проверку для статусов 400 и выше, неправильно введен пароль и так далее
+        }).catch(function (error: AxiosError) {
+            if (error) {
+                result = "Что то не так с сервером";
+            } 
+        });
+        return result;
     }
 }
 
