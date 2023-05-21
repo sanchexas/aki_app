0ccef1a1a5c24170bdcd86660b277eb5b7389dc6
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-05-21 20:00:05 +0300

    designed areas cards

--- client/src/components/AreaCard.tsx
+++ client/src/components/AreaCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
-import testImg from '../images/test.jpg';
 import PaintStarsByRate from './PaintStarsByRate';
 import ImageSlider from './ImageSlider';
+import { useState } from 'react';
 
 type AreaCardType ={
     id?: number
@@ -11,22 +11,20 @@ type AreaCardType ={
     space: number | string
 }
 const AreaCard = ({id, imgPath, title, pricePerHour, space}: AreaCardType) =>{
-let testData = ["https://pro-dachnikov.com/uploads/posts/2021-10/1633801623_12-p-kak-sdelat-doma-kak-v-studii-foto-12.jpg", "https://phonoteka.org/uploads/posts/2021-04/1619113061_25-phonoteka_org-p-podsvetka-fona-v-studii-26.jpg"]
+
+    let testData = ["https://vibirai.ru/image/720087.jpg","https://pro-dachnikov.com/uploads/posts/2021-10/1633801623_12-p-kak-sdelat-doma-kak-v-studii-foto-12.jpg", "https://phonoteka.org/uploads/posts/2021-04/1619113061_25-phonoteka_org-p-podsvetka-fona-v-studii-26.jpg"]
 
     return(
         <Link to='' className="area__card">
             <div className="area">
-                {/* <div className="area__card__img"> */}
-                    {/* <img src={`${process.env.REACT_APP_API_URL}/${imgPath}`} alt="img" /> */}
-                    {/* <img src={testImg} alt="img" /> */}
-                    
-                {/* </div> */}
-                <ImageSlider slides={testData}/> 
+                <ImageSlider slides={testData} dots/> 
                 <div className="area__card__content">
                     <div className='area__card__title'>{title}</div>
-                    <b className='area__card__price'>{pricePerHour} ₽/час</b>
                     <div className='area__card__space'>{space} м<sup>2</sup></div>
-                    <PaintStarsByRate rate={3}/>
+                    <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                        <b className='area__card__price'>{pricePerHour} ₽/час</b>
+                        <PaintStarsByRate rate={3}/>
+                    </div>
                 </div>
             </div>
         </Link>
--- client/src/pages/Areas.tsx
+++ client/src/pages/Areas.tsx
@@ -4,9 +4,7 @@ const Areas = () =>{
     return(
         <div className="areas__page">
             <div className="areas__page__header">
-                <input type="text" className="input__default" />
-                <input type="text" className="input__default" />
-                <input type="text" className="input__default" />
+                
             </div>
             <div className="areas__page__main">
                 <AreaCard title="Арт-пространство завода «Кристалл»" pricePerHour={228} space={20}/>
